f4f32099d4fb620811001432664b391964469544
javachn <java_chn@163.com>
2014-08-15 10:48:25 +0800

    aa

--- springSocket/src/main/java/x/web/controller/IndexCtrl.java
+++ springSocket/src/main/java/x/web/controller/IndexCtrl.java
@@ -1,14 +1,21 @@
 package x.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
 @Controller
 public class IndexCtrl {
+
+	@Autowired
+	private SimpMessagingTemplate template;
 
 	@RequestMapping(value = { "/" })
 	@ResponseBody
@@ -16,24 +23,26 @@ public class IndexCtrl {
 		model.addAttribute("v", "x");
 		return model;
 	}
-
+        
+	//接受socket消息
 	@MessageMapping("/hello")
-	public Model greeting(Model model) throws Exception {
-
+	public void greeting() throws Exception {
 		System.out.println(".....server..MessageMapping..");
-
-		Thread.sleep(3000);
-		model.addAttribute("t", System.currentTimeMillis());
-		return model;
+		updatePriceAndBroadcast();
 	}
-
-	@SendTo("/topic/greetings")
+	
+	//接受socket消息，并给clien发送消息
+	@MessageMapping("/hello2")
+	@SendTo("/topic/send")
 	public Long push() throws Exception {
-
 		System.out.println(".....server..SendTo..");
-		
-		Thread.sleep(1000);
-		return System.currentTimeMillis();
+        return System.currentTimeMillis();
+	}
+	
+	//给客户端发送消息
+	private void updatePriceAndBroadcast() {
+		System.out.println(".....server..updatePriceAndBroadcast..");
+		template.convertAndSend("/topic/msg", System.currentTimeMillis());
 	}
 
 }
